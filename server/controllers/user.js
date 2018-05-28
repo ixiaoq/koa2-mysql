28fae316ca54137ee21bb103a78f94934edee2c8
yaoxiaoqiang <372632349@qq.com>
2018-05-28 17:33:06 +0800

    sql

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -6,26 +6,21 @@ module.exports = {
         let sql = `SELECT * FROM user_info`;
 
         let data = await query(sql);
-        ctx.rest(data);
+        ctx.rest({
+            success: true,
+            data: data
+        });
     },
 
     "GET /api/user/:id": async (ctx, next) => {
-        console.log(ctx.params.id);
-
         let sql = `SELECT * FROM user_info WHERE id=?`;
 
         let data = await query(sql, ctx.params.id);
-        console.log(data);
-
-        if ( data.length < 1 ) {
-            ctx.rest({
-                success: true,
-                error_msg: "参数错误, id为number类型！"
-            });
-            return;
-        }
         
-        ctx.rest(data);
+        ctx.rest({
+            success: true,
+            data: data
+        });
     }
 
 };
